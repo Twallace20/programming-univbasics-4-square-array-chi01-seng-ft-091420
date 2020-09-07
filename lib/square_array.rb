2b02fd22519aef8f93300b61e7da662bfad03b30
Tyler Wallace <tywallace2017@gmail.com>
2020-09-07 01:30:19 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,10 +3,10 @@ count = 0
 square_array = [numbers_now]
 
 while count < numbers.length do 
-  
+end 
 square_array = [9, 10, 16, 25]
 count = 0 
 while count < square_array.length do 
 square_array[count] = [square_array[count] * square_array[count]]
-count += 1 
-#end 
+count += 0 
+end 
